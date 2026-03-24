6d2b2ee08dd6a7e927cc1a03504d46d3702a4c90
EeveelutionaryBiologist <christopher.klapproth@protonmail.com>
2026-03-24 16:03:52 +0100

    Solve and upload Day4 (part 1 + 2)

---
+++ Day4/Part2/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func parse_puzzle_input(filepath string) ([][]rune, error) {
+	var matrix [][]rune
+
+	file, err := os.Open(filepath)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		row := []rune(line)
+		matrix = append(matrix, row)
+	}
+
+	return matrix, nil
+}
+
+func count_neighbors(matrix [][]rune, i int, j int) uint16 {
+	neighbors_i := [8]int{-1, -1, -1, 0, 0, 1, 1, 1}
+	neighbors_j := [8]int{-1, 0, 1, -1, 1, -1, 0, 1}
+	var neighbor_count uint16
+
+	for n := 0; n < 8; n++ {
+		this_i := i + neighbors_i[n]
+		this_j := j + neighbors_j[n]
+
+		if this_i < 0 || this_j < 0 {
+			continue
+		}
+		if this_i >= len(matrix) || this_j >= len(matrix[0]) {
+			continue
+		}
+		if matrix[this_i][this_j] == '@' {
+			neighbor_count++
+		}
+	}
+
+	return neighbor_count
+}
+
+func filter_by_neighbors(matrix [][]rune) uint32 {
+	var count uint32
+
+	// For Part II we loop till there are no changes anymore
+	for {
+		change_in_loop := false
+
+		for i, row := range matrix {
+			for j, char := range row {
+				if char == '.' {
+					// fmt.Printf("%c", char)
+					continue
+				}
+				// IF a scroll is accessible (< 4 direct neighbors) it is removed immediately
+				if count_neighbors(matrix, i, j) < 4 {
+					// fmt.Printf("x")
+					matrix[i][j] = '.'
+					change_in_loop = true
+					count++
+				}
+			}
+		}
+		if !change_in_loop {
+			break
+		}
+	}
+
+	return count
+}
+
+func main() {
+	matrix, err := parse_puzzle_input("puzzle_input.txt")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	scrolls := filter_by_neighbors(matrix)
+	fmt.Printf("Scrolls with less than 4 neighbors: %d\n", scrolls)
+}
